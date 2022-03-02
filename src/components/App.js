5f72dd5cf30b901a1d60914955534bf079ed287c
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2022-03-01 17:06:03 -0800

    refactor to remove styled components and add sass

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/style.scss';
+import InputBox from "./InputBox";
+
+
+function App() {
+  return (
+    <main>
+      <h1 className="title doodle">Can My Dog Eat This?</h1>
+      <InputBox />
+    </main>
+  );
+}
+
+
+export default App;
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,76 +1,35 @@
 import React from "react";
-import styled from "styled-components";
-import { useState, useEffect } from "react";
+//import { useState } from "react";
+import { useForm } from 'react-hook-form'
 
 const InputBox = () => {
-  const acceptableItems = ["blueberries", "lettuce", "carrots"];
-  const offLimits = ["grapes", "chocolate", "onion"];
-  const [value, setValue] = useState("");
-  let [results, setResults] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  //state
+  // const [value, setValue] = useState("");
+  // let [results, setResults] = useState("");
+  // const [submitted, setSubmitted] = useState(false);
+  
+  //lists of items
+  // const acceptableItems = ["blueberries", "lettuce", "carrots"];
+  // const offLimits = ["grapes", "chocolate", "onion"];
 
-  const handleInputChange = (event) => {
-    setValue(event.target.value);
-    console.log(value);
-  };
+  //event handlers
+const {register, handleSubmit} = useForm();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const test = event.target.value;
-    console.log(test);
-    if (acceptableItems.indexOf(test) > -1) {
-      setResults("dogs can have it");
-    } else if (offLimits.indexOf(test) > -1) {
-      setResults("not good for dogs");
-    } else {
-      setResults("there was an error");
-    }
-    setSubmitted(true);
-  };
-
-  // useEffect(() => {
-  //   console.log("effect is run");
-  // }, [results]);
-
+  //JSX
   return (
-    <FormContainer>
       <form className="search-form" onSubmit={handleSubmit}>
         <input
+          {...register('submission')}
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
           type="text"
           className="search-bar"
           placeholder="Enter a food"
-          value={value}
-          onChange={handleInputChange}
-        ></input>
-        <button className="submit-button" type="submit">
-          Submit
-        </button>
+        />
+        <input className="submit-button" type="submit" />
       </form>
-      {submitted ? (
-        <ResultsWrapper>
-          <h2>{results}</h2>
-        </ResultsWrapper>
-      ) : null}
-    </FormContainer>
   );
 };
 
 export default InputBox;
-
-const FormContainer = styled.div`
-  border: 2px solid black;
-  display: flex;
-  flex-direction: column;
-  margin: 2rem;
-  padding: 1rem;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ResultsWrapper = styled.div`
-  border: 2px solid red;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
